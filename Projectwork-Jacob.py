7f9ff4ef3e579eb707892bc6f0314e20c592568c
Jacob Stephenson <j.steph3ns0n@googlemail.com>
2015-03-16 01:11:14 +0000

    Still Work in progress

--- Projectwork-Jacob.py
+++ Projectwork-Jacob.py
@@ -1,6 +1,9 @@
 from Tkinter import *
 from PIL import Image, ImageTk
+import random
 import Tkinter
+import tkMessageBox
+v = "" 
 def close():
     root.destroy()  # def close window
 def close_pop():
@@ -9,21 +12,76 @@ def Min():
     root.wm_state('iconic')  # def minimize window
 def Frame1():
     J1 = Tk() 
-    frame = Frame(J1, height = 500, width = 500) #Open Frame J1 with size 500x500
+    frame1 = Frame(J1, height = 500, width = 500) #Open Frame J1 with size 500x500
     t=Label(J1, text="Blah Blah Blah") # Insert text here for explanation on how to use
     t.grid(row=10, column=0) #Definining position of text
     t.pack()
-    frame.pack() #Leave at end or breaks
+    frame1.pack() #Leave at end or breaks
 def Frame2():
     J2 = Tk()
-    frame = Frame(J2, bg="Red", height = 500, width = 500)
-    frame.pack() #Leave at end or breaks
+    frame2 = Frame(J2, bg="Red", height = 500, width = 500)
+    frame2.pack() #Leave at end or breaks
+def FrameSIM():
+    J4 = Tk()
+    frame4 = Frame(J4, height = 200, width = 200)
+    #Insert Simplify page under here
+    enterA = Entry(frame4)
+    Aget=enterA.get()
+    enterA.grid(row=1, column=1)
+    enterB = Entry(frame4)
+    Bget=enterB.get()
+    enterB.grid(row=1, column=2)
+    enterC = Entry(frame4)
+    Cget=enterC.get()
+    enterC.grid(row=1, column=3)
+    #################################################################################################### WIP
+    #global A
+    #A = enterA.get()
+    #global B
+   #B = enterB.get()
+    #global C
+   # C = enterC.get()
+    def Factor(a,b,c):
+        Flaggy = True
+        while Flaggy == True:
+            
+            random_a1=random.randint(-10,10)
+            random_a2=random.randint(-10,10)
+            random_c1=random.randint(-10,10)
+            random_c2=random.randint(-10,10)
+            if random_a1==0 or random_a2 == 0 or random_c1 == 0 or random_c2 == 0:
+                pass
+            elif (random_a1*random_c2) + (random_a2*random_c1) == b and random_a1/random_c1 != random_a2/random_c2 and random_a1*random_a2==a and random_c1*random_c2==c:
+                break
+                Flaggy = False
+        print "y=(%dx+(%d))(%dx+(%d))" % (random_a1,random_c1,random_a2,random_c2)
+        top = Toplevel()
+        top.title("Answer")
+        msg = Message(top, text = "y=(%dx+(%d))(%dx+(%d))" % (random_a1,random_c1,random_a2,random_c2))
+        msg.pack(side=TOP)
+
+    
+
+    Factorer = lambda x = Aget, y = Bget, z = Cget: Factor(x,y,z)
+    ###################################################################################################  WIP    
+    buttonSim1 = Tkinter.Button(frame4, text="Convert", command=Factorer)
+    buttonSim1.grid(row=2, column=3)
+    output = Entry(frame4)
+    output.grid(row=3, column=3)
+
+
+  
+    frame4.pack() #Leave at end or breaks
 def Frame3():
     J3 = Tk()
-    frame = Frame(J3, bg="Green", height = 500, width = 500)
-    #Insert Graph page here
+    frame3 = Frame(J3, height = 600, width = 1000)
+    #Insert Graph page under here
+    buttonSim = Tkinter.Button(frame3, text="Equation Convertor", command=FrameSIM)
+    buttonSim.grid(row=2, column=6, padx=10, pady=10)
+    frame3.pack() #Leave at end or breaks
 
-    frame.pack() #Leave at end or breaks
+
+
 
 
 im = Image.open('Title.jpg') #Import Image (Needs to be same directory)
